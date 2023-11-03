2bacb5b5c084f84c2bfbd68c8b0f94eb08799e0c
Kadambini30 <kadambinitrivedi@gmail.com>
2023-11-03 20:39:34 +0530

    Pills Page

--- src/views/Name.jsx
+++ src/views/Name.jsx
@@ -5,7 +5,7 @@ function Otp() {
 		<div className='flex flex-col flex-shrink-0 justify-center items-center'>
             <div className='text-center font-semibold font-family text-txt text-3xl pt-48'>Enter your name</div>
             <div className='text-center'><input className='w-64 h-14 shadow-xl mt-1 rounded-2xl text-xl bg-secondary'  type="text" /></div>
-            <div className=" bg-[#3A620C] text-white h-4 w-64 pb-10 pt-4 mt-10 text-center font-medium shadow-2xl  rounded-xl text-center text-xl ">
+            <div className=" bg-[#3A620C] text-white h-4 w-64 pb-10 pt-4 mt-10  font-medium shadow-2xl  rounded-xl text-center text-xl ">
 				<button> Next</button>
 			</div>
         </div>
---
+++ src/views/Pills.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import Gallery from './gallery.png'
+function Pills() {
+	return (
+        <>
+		<div className="pt-28 flex flex-col justify-evenly items-center gap-14">
+			 <div><button className="w-52 h-52 bg-secondary rounded-2xl shadow-2xl">
+                <div className='text-9xl text-[#2D5500] text-center pb-8'>+</div>
+            </button> </div>
+            <div>
+            <button className="w-52 h-52 pl-14 bg-secondary rounded-2xl shadow-2xl">
+                <img className='w-24 h-24'  src={Gallery} alt="" />
+            </button>
+            </div>
+		</div>
+        </>
+	);
+}
+
+export default Pills;
