1ce60805c431ea2604fd4df6ad0a0f5296564d99
Lenin Kennedy <leninkennedy4work@gmail.com>
2023-11-04 00:01:24 +0530

    change

--- src/main.jsx
+++ src/main.jsx
@@ -74,7 +74,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/* <RouterProvider router={router} /> */}
-    <Stash />,
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
