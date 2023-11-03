579fea1191508c9d133f7d0911a8c02c8ab6186a
Kadambini30 <kadambinitrivedi@gmail.com>
2023-11-03 22:19:49 +0530

    Add devices page

---
+++ src/views/adddevices.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function adddevices() {
+  return (
+    <div className='flex flex-col justify-center items-center h-2/3 w-screen'>
+      <p className='text-txt text-4xl font-semibold'>Connect Your Device</p>
+      <button className='bg-secondary w-60 radius-xl text-txt rounded-xl mt-10 h-10 font-semibold text-2xl'>Add</button>
+    </div>
+  )
+}
+
+export default adddevices
