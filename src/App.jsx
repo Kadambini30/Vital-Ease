f72afa91352c67c07159dd6f50013dafc71af08f
Kadambini30 <kadambinitrivedi@gmail.com>
2023-11-03 22:16:32 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 import Landing from './views/Landing'
 import SignUp from './views/SignUp'
 import SignIn from './views/SignIn'
+import Name from './views/Name'
+import AddContact from './views/AddContact'
 import Otp from './views/Otp'
+import Emergency from './views/Emergency'
+import EmergencyContacts1 from './views/EmergencyContacts1'
+import Welcom from './views/Welcom'
+import Pills from './views/Pills'
+import Contacts from './views/Contact'
+import Sos from './views/Sos'
 import { useState } from "react";
 import TLogo from './views/text-Logo.png'
 import "./App.css";
@@ -18,10 +26,18 @@ const viewMap = {
   signup:<SignUp/>,
   signin:<SignIn/>,
   otp:<Otp/>,
+  name:<Name/>,
+  addcontact:<AddContact/>,
+  emergency:<Emergency/>,
+  emergencycontacts1:<EmergencyContacts1/>,
+  welcom:<Welcom/>,
+  pills:<Pills/>,
+  contacts:<Contacts/>,
+  sos:<Sos/>
 };
 
 function App() {
-  const [view, setView] = useState("otp");
+  const [view, setView] = useState("sos");
   return (
     <div className='bg-primary w-screen h-screen'>
         <div><img className='h-26 w-28 pl-4 pt-4' src={TLogo} alt="" /></div>
